02367d2c435d596983183e77f8e7fb6d80e57411
Concon <meetky@sina.cn>
2022-01-20 08:44:56 +0800

    emmm随便改了啥

---
+++ common/handle_path.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+******************************
+ @Author   : Con
+ @Email    : meetky@sina.cn
+ @Project  : interface_testframwork
+ @Date     : 2022-01-19 22:07
+ @File     : handle_path.py
+ @Description: 
+ @Software : PyCharm
+******************************
+"""
+import os
+
+BASE_PATH = os.path.abspath(__file__)
+print(BASE_PATH)
---
+++ conf/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+******************************
+ @Author   : Con
+ @Email    : meetky@sina.cn
+ @Project  : interface_testframwork
+ @Date     : 2022-01-19 22:07
+ @File     : __init__.py
+ @Description: 
+ @Software : PyCharm
+******************************
+"""
